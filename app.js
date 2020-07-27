a1dfbc256d87b89c29e6fdec59280352d691ee56
Melanie Gilman <melaniegilman259@yahoo.com>
2020-07-27 18:45:24 -0400

    installing inquirer

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
-// console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-// const printProfileData = (profileDataArr) => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('================');
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+// const pageHTML = generatePage(portfolioData);
 
-    console.log('Portfolio complete! Check out index.html to see the ouput!');
-});
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the ouput!');
+// });
 
